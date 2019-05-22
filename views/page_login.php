c0712c4c41bf32b65f64b7af1f8039f9facb219e
webinar-demo-kat <50562056+webinar-demo-kat@users.noreply.github.com>
2019-05-22 22:24:01 +0530

    Update page_login.php

--- views/page_login.php
+++ views/page_login.php
@@ -43,7 +43,7 @@
                     </div>
                     <div class="form-group">
                         <div class="col-sm-offset-4 col-sm-8">
-                            <button id="btn-login-abc" type="submit" class="btn btn-default">Login ABC</button>
+                            <button id="btn-login" type="submit" class="btn btn-default">Login</button>
                         </div>
                     </div>
                 </form>
